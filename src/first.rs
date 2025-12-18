6c0d2f77cbf646567957cc8470c8883f1efa7d75
Calvin D. <cdevogel25@gmail.com>
2025-12-18 10:54:09 -0600

    help me

--- src/first.rs
+++ src/first.rs
@@ -19,7 +19,7 @@ impl List {
         List {head: Link::Empty}
     }
 
-    pub fn psuh(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
             next: mem::replace(&mut self.head, Link::Empty),
@@ -29,13 +29,53 @@ impl List {
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match self.head {
-            Link::Empty => {
-
-            }
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
             Link::More(node) => {
-
+                self.head = node.next;
+                Some(node.elem)
             }
-        };
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // check pop on empty list
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // check pop on non-empty list
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // push more elements
+        list.push(4);
+        list.push(5);
+
+        // more normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // exhaust
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
